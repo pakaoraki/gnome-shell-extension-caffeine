45187b5375f3ee10044eb012a0e239bb2135dac6
pakaoraki <pakaoraki@gmx.com>
2024-04-23 22:11:15 +0200

    Fix syntax

--- .eslint.config.js
+++ .eslint.config.js
@@ -4,18 +4,20 @@ import gjsConfig from "./lint/eslintrc-gjs.config.js";
 import shellConfig from "./lint/eslintrc-shell.config.js";
 
 export default [
-  gjsConfig,
-  shellConfig,
   {
-    parserOptions: {
-      sourceType: 'module',
-    },
-    rules: {
-      // Rules overridden to match this codebase
-      'arrow-parens': ['error', 'always'],
-      'comma-dangle': ['error', 'never'],
-      curly: ['error', 'all'],
-      'prefer-template': 'off',
-    },
+    gjsConfig,
+    shellConfig,
+    {
+      parserOptions: {
+        sourceType: 'module',
+      },
+      rules: {
+        // Rules overridden to match this codebase
+        'arrow-parens': ['error', 'always'],
+        'comma-dangle': ['error', 'never'],
+        curly: ['error', 'all'],
+        'prefer-template': 'off',
+      },
+    }
   }
 ];
