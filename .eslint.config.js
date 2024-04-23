e1048783ed369c05cd6ef1849e54054e4cc436ad
pakaoraki <pakaoraki@gmx.com>
2024-04-23 21:34:50 +0200

    Replace extends property

--- .eslint.config.js
+++ .eslint.config.js
@@ -1,17 +1,20 @@
 // eslint.config.js
+import gjsConfig from './lint/eslintrc-gjs.config.js';
+import shellConfig from './lint/eslintrc-shell.config.js';
+
 module.exports = {
-  extends: [
-    './lint/eslintrc-gjs.config.js',
-    './lint/eslintrc-shell.config.js',
-  ],
-  parserOptions: {
+  gjsConfig,
+  shellConfig,
+  {
+    parserOptions: {
     sourceType: 'module',
-  },
-  rules: {
-    // Rules overridden to match this codebase
-    'arrow-parens': ['error', 'always'],
-    'comma-dangle': ['error', 'never'],
-    curly: ['error', 'all'],
-    'prefer-template': 'off',
-  },
+    },
+    rules: {
+      // Rules overridden to match this codebase
+      'arrow-parens': ['error', 'always'],
+      'comma-dangle': ['error', 'never'],
+      curly: ['error', 'all'],
+      'prefer-template': 'off',
+    },
+  }
 };
